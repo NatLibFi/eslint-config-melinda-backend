a6d471be074432266040ead962f3dfc4ae5d71cd
Minttu Hurme <ammsalme@users.noreply.github.com>
2023-09-21 15:54:11 +0300

    Revert "Add parserOptions.project"
    This reverts commit a95d20c37a6937d3782689a3774f10f412debe6f.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 *
 * ESLint config for Melinda's backend applications
 *
-* Copyright (C) 2020-2023 University Of Helsinki (The National Library Of Finland)
+* Copyright (C) 2020-2022 University Of Helsinki (The National Library Of Finland)
 *
 * This file is part of eslint-config-melinda-backend
 *
@@ -28,8 +28,7 @@
 
 module.exports = {
     parserOptions: {
-        ecmaVersion: '2020',
-        project: './tsconfig.json'
+        ecmaVersion: '2020'
     },
     parser: '@babel/eslint-parser',
     env: {
