2191a9f55c3d987962541525d6a8f0bf8d2cb75c
Minttu Hurme <ammsalme@users.noreply.github.com>
2023-09-21 15:52:48 +0300

    Revert "Remove no-conditional-statements"
    This reverts commit 40b43f07239c53514d17d4b2cf7f48c7a205186a.

--- src/index.js
+++ src/index.js
@@ -291,6 +291,12 @@ module.exports = {
         'functional/immutable-data': 'error',
         'functional/no-let': 'error',
         'functional/no-this-expressions': 'error',
-        'functional/no-loop-statements': 'error'
+        'functional/no-loop-statements': 'error',
+        'functional/no-conditional-statements': [
+            'error',
+            {
+                'allowReturningBranches': true
+            }
+        ]
     }
 };
